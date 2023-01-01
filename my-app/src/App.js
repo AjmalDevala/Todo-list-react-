91cf8e567d6581a14973c3ad93a0fdf8490cbf62
AjmalDevala <ajmaldevala@gmail.com>
2023-01-01 17:18:32 +0530

    todoapp

---
+++ my-app/src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import {useState} from 'react'
+ 
+function App() {
+  const [toDos,setTodos]= useState([])
+  const [toDo,setTodo] = useState('')
+  return (
+    <div className="app">
+    <div className="mainHeading">
+      <h1>TODO LIST 🎫</h1>
+    </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+      <i onClick={()=>setTodos([...toDos,{id :Date.now() ,text: toDo ,status :false}])} className="fas fa-plus"></i>
+    </div>
+    <div className="todos">
+     { toDos.map((obj)=>{
+      return(
+       <div className="todo">
+        <div className="left">
+          <input onChange={(e)=>{
+           setTodos(toDos.filter(obj2=>{
+            if(obj2.id===obj.id){
+              obj2.status=e.target.checked
+            }
+            return obj2
+           }))
+          
+          }}value={obj.status} type="checkbox" name="" id="" />
+          <p>{obj.text}</p>
+        </div>
+        <div className="right">
+          <i className="fas fa-times"
+          onClick={(e)=>{
+            setTodos(toDos.filter((toDo)=>{
+              if(toDo.id !== obj.id){
+                return toDo
+              }
+              return null
+            }))
+          }}></i>
+        </div>
+      </div>
+      )
+     })}
+
+     {toDos.map((obj)=>{
+      if(obj.status){
+        return(<h1>{obj.text}</h1>)
+      }
+      return null
+     })
+
+     }
+     </div>
+  </div>
+  );
+}
+
+export default App;
